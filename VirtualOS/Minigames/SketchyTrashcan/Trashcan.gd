124339a765f4afe066338c7b8e330b708094c8e8
VinegarLove <masso912@gmail.com>
2019-03-10 12:11:14 +0100

    [SketchyTrashcan] basic objects structure

--- VirtualOS/Minigames/SketchyTrashcan/Trashcan.gd
+++ VirtualOS/Minigames/SketchyTrashcan/Trashcan.gd
@@ -1,14 +1,19 @@
 extends Sprite
 
+onready var timer = $FleeCooldown
+onready var ray_down = $RayDown
+onready var ray_left = $RayLeft
+onready var ray_right = $RayRight
+onready var ray_up = $RayUp
+
 func _ready():
 	self.father_node = __get_father_node()
 	self.bounds = __get_bounds()
 
-func is_able_to_move():
-	pass
+func flee_from_threat():
+	# flee stuff...
 
-func flee_from_threat(delta):
-	pass
+	__start_flee_cooldown()
 
 func __get_father_node():
 	return get_node("../")
@@ -16,9 +21,22 @@ func __get_father_node():
 func __get_bounds():
 	return __get_father_node().get_rect()
 
-func __is_still_fleeing():
-	pass
-
 func _callback_folder_enter_zone():
-	if not __is_still_fleeing():
-		pass
+	if __can_flee():
+		flee_from_threat()
+
+
+
+func _on_FleeCooldown_timeout():
+	# let the bin flee again
+	__enable_flee()
+
+func __enable_flee():
+	self.can_flee = true
+
+func __start_flee_cooldown():
+	self.can_flee = false
+	timer.start()
+
+func __can_flee():
+	return self.can_flee
