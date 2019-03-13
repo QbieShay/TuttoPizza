2f7a672c532d2c0b648f4060fdada03ce17f234e
VinegarLove <masso912@gmail.com>
2019-03-13 01:58:40 +0100

    Movimenti per il cestino, callback clippy, sistemati collider

--- VirtualOS/Minigames/SketchyTrashcan/SketchyTrashcan.gd
+++ VirtualOS/Minigames/SketchyTrashcan/SketchyTrashcan.gd
@@ -1,15 +1,19 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int) var first_advice_mistake_count = 2
+export(int) var second_advice_mistake_count = 5
+export(int) var solution_advice_mistake_count = 10
+
+onready var trashcan = $Trashcan
+onready var mistakes_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hint_clippy_intro()
 
 
-# clippy stuff
+############### clippy stuff ###############
+# Every function offered by the clippy singleton will need a timer or a queue on the clippy singleton to avoid spam
 func hint_clippy_intro():
 	pass
 func hint_clippy_wrong_file_deleted():
@@ -21,6 +25,14 @@ func hint_clippy_second_advice():
 func hint_clippy_solution():
 	pass
 
+func _on_Trashcan_player_mistake():
+	mistakes_count+=1
+	if mistakes_count >= solution_advice_mistake_count:
+		hint_clippy_solution()
+	elif mistakes_count >= second_advice_mistake_count:
+		hint_clippy_second_advice()
+	elif mistakes_count >= first_advice_mistake_count:
+		hint_clippy_first_advice()
 
-func get_scene_bounds():
-	return self.get_rect()
+func _on_Trashcan_wrong_file_deleted():
+	hint_clippy_wrong_file_deleted()
--- VirtualOS/Minigames/SketchyTrashcan/Trashcan.gd
+++ VirtualOS/Minigames/SketchyTrashcan/Trashcan.gd
@@ -1,42 +1,71 @@
-extends Sprite
+extends KinematicBody2D
 
-onready var timer = $FleeCooldown
-onready var ray_down = $RayDown
-onready var ray_left = $RayLeft
-onready var ray_right = $RayRight
-onready var ray_up = $RayUp
+onready var flee_cooldown = $FleeCooldown
+onready var flee_interpolation = $FleeCooldown
+onready var flee_range = $FleeCollider
+onready var bin_range = $Collider
+onready var can_flee = true
+onready var is_moving = false
+onready var path_checker = $PathChecker
+onready var interpolator = $SpeedInterpolation
+onready var current_point = -1
+onready var flee_points = get_tree().get_nodes_in_group("trashcan_flee_points")
+
+signal player_mistake
+signal wrong_file_deleted
 
 func _ready():
-	self.father_node = __get_father_node()
-	self.bounds = __get_bounds()
+	randomize()
 
-func flee_from_threat():
-	# flee stuff...
+func _process(delta):
+	if can_flee and not is_moving and __needs_to_flee():
+		__flee_from_threat()
 
+func __needs_to_flee():
+	# stub for now
+	return true
+
+func __pick_a_point_to_go():
+		# get a random point from the array
+	var cur_size = flee_points.size()
+	var point = randi() % cur_size
+
+	# address to avoid chosing the same point we are now
+	if point == current_point:
+		print("same_point")
+		point = point-1 if point > 0 else point+1
+	return point
+
+func __flee_from_threat():
 	__start_flee_cooldown()
 
-func __get_father_node():
-	return get_node("../")
+	var point = __pick_a_point_to_go()
 
-func __get_bounds():
-	return __get_father_node().get_rect()
+	# cast the ray to that point
+	path_checker.cast_to = flee_points[point].position
+	path_checker.force_raycast_update()
 
-func _callback_folder_enter_zone():
-	if __can_flee():
-		flee_from_threat()
+	# travel to destination, whether it's a folder or a point
+	var destination = path_checker.get_collision_point() if path_checker.is_colliding() else flee_points[point].position
+	interpolator.interpolate_property(self, 'position', self.position, destination, 1.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	interpolator.start()
+	is_moving = true
 
+	# set the current chosen point
+	current_point = point
 
+	# notify of the player mistake
+	emit_signal("player_mistake")
+
+func __start_flee_cooldown():
+	can_flee = false
+	flee_cooldown.start()
 
 func _on_FleeCooldown_timeout():
-	# let the bin flee again
 	__enable_flee()
 
 func __enable_flee():
-	self.can_flee = true
+	can_flee = true
 
-func __start_flee_cooldown():
-	self.can_flee = false
-	timer.start()
-
-func __can_flee():
-	return self.can_flee
+func _on_SpeedInterpolation_tween_completed(object, key):
+	is_moving = false
